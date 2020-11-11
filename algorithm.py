8597cdb8d5757f425f753b5d9dee41dcb3faa1f3
root <root@DESKTOP-2H3HUIP.localdomain>
2020-11-11 18:01:38 +0100

    done

--- algorithm.py
+++ algorithm.py
@@ -14,29 +14,33 @@ C = 20
 M = 10
 #Wartość początkowa
 tau_0 = 0.1
-#Inicjalizacja algorytmu
-G = [[1,0,1],\
-    [1,1,1],\
-    [1,1,1]
-    ]
-#Metryka
-Metric = [[5,0,3],[3,2,1],[3,2,1]]
 
-###NIE EDYTUJEMY PARAMETRÓW###
+#WAŻNE !!! jeśli w grafie nie ma połączenie i jest 0 to w metryce też musi być 0
+#Ustalenie istniejących ścieżek w grafie
+G = [[1,0,1],\
+     [1,1,1],\
+     [0,1,1]]
+#Metryka
+Metric = [[5,0,3],\
+          [3,2,1],\
+          [0,2,1]]
+
+###NIE EDYTUJEMY KOLEJNYCH PARAMETRÓW###
 #liczba kolumn
 D = len(G[0])
 #liczba wierszy
 R = len(G)
-#Tetha inizjalizacja
+#tau inizjalizacja
 tau = zeros((R,D))
 tau[...] = [tau_0]
-tau_all = [copy.deepcopy(tau) for i in range(0,M)] 
+#tau k inizjalizacja
+tau_all = [copy.deepcopy(tau) for i in range(0,M)]
+#tau best inizjalizacja
 tau_best = zeros((R,D))
-#Lista Je przechowuje tuple (t,Je) gdzie t to iteracja Je to wartość funckji celu w danej iteracji
-Je = []
-#Wartość Je_best best przechowuje [K,Je_best] gzie K to lista przechwoująca węzły ścieżki a Je Best to wartość funkcji celu
-Je_best = [0]  
+#Wartość Je_best best przechowuje [t,Je_best] t kolejna iteracja a Je_best to wartość funkcji celu
+Je_best = [(0,0)]  
 
+#definicja funkcji celu na chwilę obecną równomiernie rozprowadza feromony na węzły które należą do najlepszej ścieżki
 def goal_function(K):
     sum = 0
     for j, i in K:
@@ -48,6 +52,9 @@ def goal_function(K):
     Je = sum/D
     return Je
 
+#TODO do wymyślenia funkcja heurystyczna na chwilę obecną zwraca wartość metryki
+def get_eta_ij(t,j,i):
+        return Metric[j][i]
 def set_delta_tau_k_ij(t,k,K,Je):
     for j, i in K:
         if G[j][i] and Je:
@@ -55,16 +62,15 @@ def set_delta_tau_k_ij(t,k,K,Je):
         else:
             tau_all[k][j][i] = 0
 
-def set_delta_tau_best_ij(K,Je):
-        Je_best[0] = Je
+def set_delta_tau_best_ij(t,K,Je):
+    if Je_best[-1][1] == 0 or Je_best[-1][1] > Je:
+        Je_best.append((t,Je))
         tau_best = zeros((R,D))
         for j, i in K:
             tau_best[j][i] = Je
+    else:
+        Je_best.append((t,Je_best[-1][1]))
         
-#TODO do wymyślenia funkcja heurystyczna
-def get_eta_ij(t,j,i):
-        return Metric[j][i]
-
 #TODO pytanie czy zapamiętujemy wartość tablicy feromonów po każdej iteracji
 def set_tau_ij(t,j,i):
     def tau_func(k):
@@ -77,6 +83,34 @@ def get_p_k_ij(t,k,j,i):
         return pow(tau[x][i],alfa)*pow(get_eta_ij(t,x,i),beta)
     return pow(tau[j][i],alfa)*pow(get_eta_ij(t,j,i),beta)/new_sum(R,tau_func)
 
+def run_aco_algorithm():
+    elements = [e for e in range(0,R)]
+    #ilość cykli
+    for t in range(1, C+1):
+        #ilość mrówek
+        for k in range(0,M):
+            #losowanie ścieżek
+            K = []
+            for i in range(0,D):
+                weights = []
+                for j in range (0,R):
+                    weights.append(get_p_k_ij(t,k,j,i))
+                j = roulette_selection(elements,weights)
+                K.append((j,i))
+            Je = goal_function(K)
+            #Pozostawienie feromonu po przejściu pierwszej ścieżki
+            set_delta_tau_k_ij(t,k,K,Je)
+            #sprawdzamy czy najlepsza ścieżka do tej pory
+            set_delta_tau_best_ij(t,K,Je)
+        #ustawiamy tablice feromonuów
+        for i in range(0,D):
+            for j in range(0,R):
+                set_tau_ij(t,j,i) 
+
+#usunięcie duplikatu w tuplach i posortowanie względem iteracji
+def removeDuplicates(lst):   
+    return sorted([t for t in (set(tuple(i) for i in lst))], key=lambda tup : tup[0])
+
 #implementacja algorytmu ruletki
 def roulette_selection(elements, weights):
     return choice(elements, p=weights)
@@ -88,35 +122,8 @@ def new_sum(N, fun):
         sum += fun(i)
     return sum
 
-def run_aco_algorithm():
-    elements = [e for e in range(0,R)]
-    #ilość cykli
-    for t in range(0, C):
-        #ilość mrówek
-        for k in range(0,M):
-            #losowanie ścieżek
-            K = []
-            for i in range(0,D):
-                weights = []
-                for j in range (0,R):
-                    weights.append(get_p_k_ij(t,k,j,i))
-                j = roulette_selection(elements,weights)
-                K.append((j,i))
-            je = goal_function(K)
-            #Pozostawienie feromonu po przejściu pierwszej ścieżki
-            set_delta_tau_k_ij(t,k,K,je)
-            #sprawdzamy czy najlepsza ścieżka do tej pory
-            if Je_best[0] == 0 or Je_best[0] > je:
-                set_delta_tau_best_ij(K,je)
-        #ustawiamy tablice feromonuów
-        for i in range(0,D):
-            for j in range(0,R):
-                set_tau_ij(t,j,i)
-        Je.append((t,je))
-    return tau
-
 if __name__ == "__main__":
-    tau = run_aco_algorithm()
+    run_aco_algorithm()
     #print tablicy feromonów
     print(tau)
-    
+    print(removeDuplicates(Je_best))
