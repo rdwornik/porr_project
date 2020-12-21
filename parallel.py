fbcc4e06d0a41db7b2d81dcd56d9bff5d959f45b
robertdwornik <robert.dwornik@outlook.com>
2020-12-21 21:20:46 +0100

    parallel

--- parallel.py
+++ parallel.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pymp
 import copy
 from numpy.random import choice
 from numpy import zeros
@@ -33,14 +34,23 @@ D = len(G[0])
 #liczba wierszy
 R = len(G)
 #tau inizjalizacja
-tau = zeros((R,D))
-tau[...] = [tau_0]
+
+# tau_tmp = zeros((R,D))
+# tau_tmp[...] = [tau_0
+
+tau = pymp.shared.array(np.array((R,D)))
+tau[...] = tau_0
+
 #tau k inizjalizacja
-tau_all = [copy.deepcopy(tau) for i in range(0,M)]
+tau_all = pymp.shared.array(np.array((M,R,D)))
+tau_all[...] = tau_0
+
+
 #tau best inizjalizacja
-tau_best = zeros((R,D))
+tau_best = pymp.shared.array(np.array((R,D)))
+
 #Wartość Je_best best przechowuje [t,Je_best] t kolejna iteracja a Je_best to wartość funkcji celu
-Je_best = [(0,0)]  
+Je_best = pymp.shared.list([(0,0)])
 
 #definicja funkcji celu na chwilę obecną równomiernie rozprowadza feromony na węzły które należą do najlepszej ścieżki
 def goal_function(K):
@@ -58,27 +68,30 @@ def goal_function(K):
 def get_eta_ij(t,j,i):
         return Metric[j][i]
 
-def set_delta_tau_k_ij(t,k,K,Je):
+def set_delta_tau_k_ij(t,k,K,Je,p):
+    # with p.lock:
     for j, i in K:
         if G[j][i] and Je:
             tau_all[k][j][i] = 1/Je
         else:
             tau_all[k][j][i] = 0
 
-def set_delta_tau_best_ij(t,K,Je):
-    if Je_best[-1][1] == 0 or Je_best[-1][1] > Je:
-        Je_best.append((t,Je))
-        tau_best = zeros((R,D))
-        for j, i in K:
-            tau_best[j][i] = Je
-    else:
-        Je_best.append((t,Je_best[-1][1]))
+def set_delta_tau_best_ij(t,K,Je,p):
+    with p.lock:
+        if Je_best[-1][1] == 0 or Je_best[-1][1] > Je:
+                Je_best.append((t,Je))
+                tau_best[...] = .0
+                for j, i in K:
+                    tau_best[j][i] = Je
+        else:
+            Je_best.append((t,Je_best[-1][1]))
         
 #TODO pytanie czy zapamiętujemy wartość tablicy feromonów po każdej iteracji
-def set_tau_ij(t,j,i):
+def set_tau_ij(t,j,i,p):
     def tau_func(k):
         return tau_all[k][j][i]
-    tau[j][i] = (1-rho)*tau[j][i] + new_sum(M,tau_func) + rho*tau_best[j][i]
+    with p.lock:
+        tau[j][i] = (1-rho)*tau[j][i] + new_sum(M,tau_func) + rho*tau_best[j][i]
 
 #Prawdopodobieństwo wybrania węzła j będąc na węźle i
 def get_p_k_ij(t,k,j,i):
@@ -91,24 +104,27 @@ def run_aco_algorithm():
     #ilość cykli
     for t in range(1, C+1):
         #ilość mrówek
-        for k in range(0,M):
-            #losowanie ścieżek
-            K = []
+        with pymp.Parallel(M) as p:
+            for k in p.range(0,M):
+                #losowanie ścieżek
+                K = []
+                with p.lock:
+                    for i in range(0,D):
+                        weights = []
+                        for j in range (0,R):
+                            weights.append(get_p_k_ij(t,k,j,i))
+                        j = roulette_selection(elements,weights)
+                        K.append((j,i))
+                Je = goal_function(K)
+                #Pozostawienie feromonu po przejściu pierwszej ścieżki
+                set_delta_tau_k_ij(t,k,K,Je,p)
+                #sprawdzamy czy najlepsza ścieżka do tej pory
+                set_delta_tau_best_ij(t,K,Je,p)
+            #ustawiamy tablice feromonuów
             for i in range(0,D):
-                weights = []
-                for j in range (0,R):
-                    weights.append(get_p_k_ij(t,k,j,i))
-                j = roulette_selection(elements,weights)
-                K.append((j,i))
-            Je = goal_function(K)
-            #Pozostawienie feromonu po przejściu pierwszej ścieżki
-            set_delta_tau_k_ij(t,k,K,Je)
-            #sprawdzamy czy najlepsza ścieżka do tej pory
-            set_delta_tau_best_ij(t,K,Je)
-        #ustawiamy tablice feromonuów
-        for i in range(0,D):
-            for j in range(0,R):
-                set_tau_ij(t,j,i) 
+                for j in range(0,R):
+                    set_tau_ij(t,j,i,p) 
+        
 
 #usunięcie duplikatu w tuplach i posortowanie względem iteracji
 def removeDuplicates(lst):   
@@ -124,6 +140,8 @@ def new_sum(N, fun):
     for i in range(0,N):
         sum += fun(i)
     return sum
+
+
 
 if __name__ == "__main__":
     run_aco_algorithm()
